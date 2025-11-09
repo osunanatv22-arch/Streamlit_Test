c1a88979df20eef1ff51c638e9b4cc195a5bff96
osunanatv22-arch <osunanatv22@gmail.com>
2025-11-08 19:18:05 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.header('Lanzar una moneda')
 
 st.write('Esta aplicación aún no es funcional. En construcción.')
-Vamos a confirmar los cambios para asegurarnos de que el repositorio esté actualizado:
+
 
