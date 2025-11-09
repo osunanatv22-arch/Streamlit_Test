cb55f1d0116846aa5781f18b3b2859e51d140d50
osunanatv22-arch <osunanatv22@gmail.com>
2025-11-08 19:08:27 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.header('Lanzar una moneda')
+
+st.write('Esta aplicación aún no es funcional. En construcción.')
+Vamos a confirmar los cambios para asegurarnos de que el repositorio esté actualizado:
+
